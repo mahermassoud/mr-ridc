341f0a8808b8800aab04f2eff43975cb61c7576d
massoudTheDude <mahermassoud@gmail.com>
2020-04-25 17:17:02 -0700

    drug 2 moa dict, use dosages

--- v2_clean_drugs.py
+++ v2_clean_drugs.py
@@ -30,6 +30,11 @@ ov_response = drug_response # They actually completely overlap
 #%% Drug response one where the cell lines are in crispr+rna
 feat_ov_response = ov_response[ov_response["cell_line"].isin(other_cell_lines)]
 
+#%% Make order of drugs match between info and response
+uniq_ov_info = ov_info.drop_duplicates("broad_id").sort_values("broad_id")
+#feat_ov_response = feat_ov_response.sort_values("broad_id")
+#ov_response = ov_response.sort_values("broad_id")
+
 #%% Drop duplicates from drug info and N-hot encode MOAs
 def get_n_hot_moa(uniq_drug_info):
   uniq_moa_list = uniq_drug_info["moa"].str.split(",")
@@ -39,19 +44,40 @@ def get_n_hot_moa(uniq_drug_info):
   n_uniq_moa = uniq_moa.shape[0]
 
   moa2ind = dict(zip(uniq_moa, np.arange(uniq_moa.shape[0])))
-  n_hot_moa = np.zeros((uniq_moa.shape[0], n_uniq_moa))
+  n_hot_moa = np.zeros((uniq_drug_info.shape[0], n_uniq_moa))
   list_moas = list(uniq_moa_list)
+  drug2moa_n_hot, drug2moa_inds = {}, {}
   for i in range(n_hot_moa.shape[0]):
     new_row = np.zeros((1, n_uniq_moa))
     moas = [m.strip() for m in list_moas[i] ]
     one_inds = [moa2ind[m] for m in moas]
     new_row[0,one_inds] = 1
     n_hot_moa[i,:] = new_row
+    
+    drug = uniq_drug_info["broad_id"].iloc[i]
+    drug2moa_inds[drug] = one_inds
+    drug2moa_n_hot[drug] = new_row
 
-  return uniq_drug_info["broad_id"].to_numpy(), uniq_moa, n_hot_moa
+  out = [uniq_drug_info["broad_id"].to_numpy(), uniq_moa, n_hot_moa, 
+         drug2moa_inds, drug2moa_n_hot]
+  return out
 
-uniq_ov_info = ov_info.drop_duplicates("broad_id")
-broad_id_np, uniq_moa_np, n_hot_moa = get_n_hot_moa(uniq_ov_info)
+broad_id_np, uniq_moa_np, n_hot_moa, drug2moa_inds, drug2moa_n_hot = (
+  get_n_hot_moa(uniq_ov_info))
 
+# %% Stack drug dosage onto drug_response
+def stack_response_dosage(drug_response):
+  drugs = [c.split("::")[0] for c in drug_response.columns[1:]]
+  drugs = np.array(drugs)
+  dosages = [c.split("::")[1] for c in drug_response.columns[1:]]
+  dosages = np.array(dosages).astype(float)
+
+  resp_mat_np = drug_response.iloc[:,1:].to_numpy()
+  resp_mat_np = np.transpose(resp_mat_np)
+  resp_dosage_np = np.concatenate([dosages[:,np.newaxis], resp_mat_np], axis=1)
+
+  return drug_response["cell_line"].to_numpy(), drugs, resp_dosage_np
+
+ov_resp_cell, ov_resp_drug, ov_dose_resp = stack_response_dosage(ov_response)
 
 # %%
